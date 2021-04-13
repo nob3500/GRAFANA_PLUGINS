9a80212ba0390cbd92cb4cf9fca1d97643755fc8
bryanind195 <75736079+bryanind195@users.noreply.github.com>
2021-04-13 16:08:06 -0500

    se inicia archivos para el plugin de las umas

--- grafana-plugins/pdu/src/SimplePanel.tsx
+++ grafana-plugins/pdu/src/SimplePanel.tsx
@@ -6,7 +6,10 @@ import { css, cx } from 'emotion';
 import { stylesFactory} from '@grafana/ui';
 import Pdu from './components/pdu';
 
+
 interface Props extends PanelProps<SimpleOptions> {}
+
+
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
   //const theme = useTheme();
