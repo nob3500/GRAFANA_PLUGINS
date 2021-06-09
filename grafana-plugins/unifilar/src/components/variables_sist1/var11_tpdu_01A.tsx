201b8b8dc94203fcca16851304a133179aa75bbf
bryanind195 <bryanind195@gmail.com>
2021-06-09 14:36:11 -0500

    se finaliza archivo var_13 y var_12 .tsx sin novedad se inicia archivo var11_tpdu01A.tsx

---
+++ grafana-plugins/unifilar/src/components/variables_sist1/var11_tpdu_01A.tsx
@@ -0,0 +1,59 @@
+/*import React from 'react'
+
+type TPDU_01A = {
+  
+
+//className = {tpdu01A}
+
+//Breakers T-PDU-01A
+    tpdu01A_0: string;//
+    tpdu01A_1: string;//
+    tpdu01A_2: string;//
+    tpdu01A_3: string;//
+    tpdu01A_4: string;//
+    tpdu01A_5: string;//
+    tpdu01A_6: string;//
+    tpdu01A_7: string;//
+    tpdu01A_8: string;//
+    tpdu01A_9: string;//
+    tpdu01A_10: string;//
+    tpdu01A_11: string;//
+    tpdu01A_12: string;//
+
+// indicador de PDis
+
+    pdu01AF1: string;//
+    pdu01AF2: string;//
+    pdu01AF3: string;//
+    pdu01AF4: string;//
+    pdu01AF5: string;//
+    pdu01AF6: string;//
+    pdu01AF7: string;//
+    pdu01AF8: string;//
+    pdu01AF9: string;//
+    pdu01AF10: string;//
+    pout1A: string;//
+
+
+      
+}
+export interface DataTpdu01A{
+
+  //className = {tpdu01A.atschi07A_F2}
+  tpdu01A: TPDU_01A; 
+
+}
+
+export const Var11 = ({
+    tpdu01A
+
+}: DataTpdu01A) =>{  
+
+    return (
+        //Se inserta GRupo del tablero Ats-Chi-07A
+
+       
+    )
+}
+
+*/
