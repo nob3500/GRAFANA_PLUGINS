c0ad5aa4d4e4cb0493357384526e66008f780f22
bryanind195 <bryanind195@gmail.com>
2021-06-10 12:39:50 -0500

    se finaliza el estructura de codigo del documento var5_ats_ant_sat_1A.tsx y se inicia documento var4_tdp_01A.tsx

---
+++ grafana-plugins/unifilar/src/components/variables_sist1/var5_ats_ant_sat_1A.tsx
@@ -0,0 +1,231 @@
+import React from 'react'
+
+type ATS_ANT_SAT_1A = {
+  
+
+//className = {atsantsat1A.atsantsat1A_F2}
+
+    atsantsat1A_F2:    string;//ellipse5043
+    atsantsat1A_tri2:  string;//path5063
+    atsantsat1A_F1:    string;//ellipse5049
+    atsantsat1A_tri1:  string;//path5065
+
+}
+export interface DataAtsantsat1A{
+  
+    atsantsat1A: ATS_ANT_SAT_1A; 
+
+}
+export const Var5 = ({
+    atsantsat1A
+
+}: DataAtsantsat1A) =>{  
+
+    return (
+        //Se inserta GRupo del tablero Dataatsantsat1A
+        <g id="layer30">
+      <g id="g5069" transform="matrix(.11659 0 0 .1289 42.515 37.881)">
+        <g id="g5067" transform="translate(-123.83 8.15)">
+          <g id="g4834" transform="matrix(1.5342 0 0 1.5305 -523.97 -260.69)">
+            <g
+              id="g5027"
+              transform="matrix(.86223 0 0 .24156 2119.3 568.82)"
+              clipPath="url(#clipath_lpe_path-effect5071)"
+              strokeWidth={0.99961}
+              stroke="#4a4a4a"
+            >
+              <g
+                id="g5025"
+                transform="matrix(1.0234 0 0 .99996 -67.752 -224.6)"
+                clipPath="none"
+                strokeWidth={0.98813}
+                stroke="#4a4a4a"
+              >
+                <path
+                  id="path5023"
+                  d="M-1307.3-167.17h95.668c2.816 0 5.082 2.823 5.082 6.33V75.21c0 3.506-2.266 6.33-5.082 6.33h-95.668c-2.816 0-5.083-2.824-5.083-6.33v-236.05c0-3.507 2.267-6.33 5.083-6.33z"
+                  strokeLinecap="round"
+                  strokeMiterlimit={15}
+                  strokeWidth={0.98837}
+                  stroke="#4a4a4a"
+                />
+              </g>
+            </g>
+            <g
+              id="g5033"
+              transform="matrix(.86213 0 0 .24151 2119.2 568.8)"
+              clipPath="url(#clipath_lpe_path-effect5073)"
+              strokeWidth={0.99961}
+              stroke="#4a4a4a"
+            >
+              <g
+                id="g5031"
+                transform="matrix(1.0234 0 0 .99996 -67.752 -224.6)"
+                clipPath="none"
+                strokeWidth={0.98813}
+                stroke="#4a4a4a"
+              >
+                <path
+                  id="path5029"
+                  d="M-1307.3-167.17h95.668c2.816 0 5.082 2.823 5.082 6.33V75.21c0 3.506-2.266 6.33-5.082 6.33h-95.668c-2.816 0-5.083-2.824-5.083-6.33v-236.05c0-3.507 2.267-6.33 5.083-6.33z"
+                  fill="url(#linearGradient15460)"
+                  strokeLinecap="round"
+                  strokeMiterlimit={15}
+                  strokeWidth={0.98837}
+                  stroke="#116aef"
+                />
+              </g>
+            </g>
+            <path
+              id="path5035"
+              transform="matrix(.84659 0 0 .64923 1994.8 1214)"
+              d="M-1284.2-1155.8h99.106c3.11 0 5.613 2.028 5.613 4.546v15.612c0 2.519-2.504 4.547-5.613 4.547h-99.106c-3.11 0-5.613-2.028-5.613-4.547v-15.612c0-2.518 2.504-4.546 5.613-4.546z"
+              clipPath="url(#clipath_lpe_path-effect5075)"
+              fill="url(#linearGradient15462)"
+              strokeLinejoin="round"
+              strokeWidth={1.008}
+              stroke="#116aef"
+            />
+            <path
+              id="path5037"
+              d="M902.44 476.89h94.251"
+              fill="none"
+              strokeWidth={0.52182}
+              stroke="#116aef"
+            />
+            <path
+              id="rect5039"
+              fill="#939d93"
+              paintOrder="markers stroke fill"
+              strokeWidth={0.13689}
+              stroke="#000"
+              d="M973.79 480.86H975.4449999999999V529.85H973.79z"
+            />
+            <path
+              id="path5041"
+              d="M936.88 520.42h37.14"
+              fill="#939d93"
+              strokeWidth={0.99383}
+              stroke="#939d93"
+            />
+            <ellipse
+              id="ellipse5043"
+              cx={924.56}
+              cy={492.19}
+              rx={12.034}
+              ry={11.275}
+              fill="#0f0"
+              className={atsantsat1A.atsantsat1A_F2}
+            />
+            <text
+              id="text5047"
+              transform="scale(1.1716 .85351)"
+              x={781.35291}
+              y={581.18707}
+              style={{ lineHeight: 1.25 }}
+              xmlSpace="preserve"
+              fill="#000"
+              fontFamily="sans-serif"
+              fontSize="12.539px"
+              letterSpacing={0}
+              strokeWidth={0.31348}
+              wordSpacing={0}
+            >
+              <tspan
+                id="tspan5045"
+                x={781.35291}
+                y={581.18707}
+                fill="#000"
+                strokeWidth={0.31348}
+              >
+                {"F2"}
+              </tspan>
+            </text>
+            <ellipse
+              id="ellipse5049"
+              cx={924.56}
+              cy={519.88}
+              rx={12.034}
+              ry={11.275}
+              fill="#999"
+              className={atsantsat1A.atsantsat1A_F1}
+            />
+            <text
+              id="text5053"
+              transform="scale(1.1716 .85351)"
+              x={781.09271}
+              y={614.09332}
+              style={{ lineHeight: 1.25 }}
+              xmlSpace="preserve"
+              fill="#000"
+              fontFamily="sans-serif"
+              fontSize="12.539px"
+              letterSpacing={0}
+              strokeWidth={0.31348}
+              wordSpacing={0}
+            >
+              <tspan
+                id="tspan5051"
+                x={781.09271}
+                y={614.09332}
+                fill="#000"
+                strokeWidth={0.31348}
+              >
+                {"F1"}
+              </tspan>
+            </text>
+            <path
+              id="path5061"
+              d="M936.72 491.34h37.308"
+              fill="#939d93"
+              strokeWidth={0.99383}
+              stroke="#939d93"
+            />
+            <path
+              id="path5063"
+              d="M944.74 483.14l.491 18.094 18.677-10.052z"
+              fill="#0f0"
+              className={atsantsat1A.atsantsat1A_tri2}
+            />
+            <path
+              id="path5065"
+              d="M944.74 512.07l.491 18.094 18.677-10.052z"
+              fill="#999"
+              className={atsantsat1A.atsantsat1A_tri1}
+            />
+          </g>
+          <text
+            id="text5059"
+            transform="scale(1.0191 .98121)"
+            x={846.78217}
+            y={474.26251}
+            style={{ lineHeight: 1.25 }}
+            xmlSpace="preserve"
+            fill="#fff"
+            fontFamily="sans-serif"
+            fontSize="10.86px"
+            letterSpacing={0}
+            strokeWidth={0.2715}
+            wordSpacing={0}
+          >
+            <tspan
+              id="tspan4811"
+              x={846.78217}
+              y={474.26251}
+              fill="#fff"
+              fontFamily="sans-serif"
+              fontSize="14.389px"
+              fontWeight={700}
+              strokeWidth={0.2715}
+            >
+              {"ATS-ANT-SAT-1A"}
+            </tspan>
+          </text>
+        </g>
+      </g>
+    </g>
+            
+        
+    )
+}
+
