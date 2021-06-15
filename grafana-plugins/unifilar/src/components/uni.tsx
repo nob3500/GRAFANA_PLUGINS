5de9a0f62d6d38d2f2025e24ca70b998015d3270
Angel ind <bryanind195@gmail.com>
2021-06-14 20:52:58 -0500

    se realiza estructura de codigo de archivo de variables var24 se finaliza sin novedad se inicia con el archivo var25

--- grafana-plugins/unifilar/src/components/uni.tsx
+++ grafana-plugins/unifilar/src/components/uni.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 //VARIABLES SISTEMA 2
 
+import {Var24, DataAtspdu10A} from './variables_sist2/var24_ats_pdu_10A';
 import {Var23, DataTupsout02A} from './variables_sist2/var23_tups_out_02A';
 import {Var22, DataTupsin02A} from './variables_sist2/var22_tups_in_02A';
 import {Var21, DataRec02A} from './variables_sist2/var21_rec_02A';
@@ -430,6 +431,18 @@ const uni = () => {
       tupsout02A_5: '',
       tupsout02A_6: '',
       tupsout02A_7: '',
+    }
+  }
+
+  let uni_24: DataAtspdu10A = {
+
+    atspdu10A: {
+
+      atspdu10A_F2:    '',
+      atspdu10A_tri2:  '',
+      atspdu10A_F1:    '',
+      atspdu10A_tri1:  '',
+
     }
   }
      
@@ -5525,202 +5538,7 @@ const uni = () => {
 
     
     <g id="layer7">
-      <g id="g12159" transform="translate(181.87 -.877) scale(.14245)">
-        <g id="g6182">
-          <g id="g6154" transform="translate(-108.43 -86.896) scale(1.2848)">
-            <g
-              id="g5647-8-7-0"
-              transform="matrix(.89652 0 0 .24156 1570.4 367.31)"
-              clipPath="url(#clipath_lpe_path-effect19753)"
-              strokeWidth={0.99961}
-              stroke="#4a4a4a"
-            >
-              <g
-                id="g5645-8-99-4"
-                transform="matrix(1.0234 0 0 .99996 -67.752 -224.6)"
-                clipPath="none"
-                strokeWidth={0.98813}
-                stroke="#4a4a4a"
-              >
-                <path
-                  id="path5643-5-7-3"
-                  d="M-1307.3-167.17h95.668c2.816 0 5.082 2.823 5.082 6.33V75.21c0 3.506-2.266 6.33-5.082 6.33h-95.668c-2.816 0-5.083-2.824-5.083-6.33v-236.05c0-3.507 2.267-6.33 5.083-6.33z"
-                  strokeLinecap="round"
-                  strokeMiterlimit={15}
-                  strokeWidth={0.98837}
-                  stroke="#4a4a4a"
-                />
-              </g>
-            </g>
-            <g
-              id="g5050-3-0-1-9"
-              transform="matrix(.89644 0 0 .24151 1570.3 367.29)"
-              clipPath="url(#clipath_lpe_path-effect19755)"
-              strokeWidth={0.99961}
-              stroke="#4a4a4a"
-            >
-              <g
-                id="g4980-3-4-3-7-5"
-                transform="matrix(1.0234 0 0 .99996 -67.752 -224.6)"
-                clipPath="none"
-                strokeWidth={0.98813}
-                stroke="#4a4a4a"
-              >
-                <path
-                  id="rect4958-5-5-9-1-7"
-                  d="M-1307.3-167.17h95.668c2.816 0 5.082 2.823 5.082 6.33V75.21c0 3.506-2.266 6.33-5.082 6.33h-95.668c-2.816 0-5.083-2.824-5.083-6.33v-236.05c0-3.507 2.267-6.33 5.083-6.33z"
-                  fill="url(#linearGradient5490)"
-                  strokeLinecap="round"
-                  strokeMiterlimit={15}
-                  strokeWidth={0.98837}
-                  stroke="#4a4a4a"
-                />
-              </g>
-            </g>
-            <path
-              id="rect9071-6-6-2-6-7-2"
-              transform="matrix(.88028 0 0 .72138 1440.9 1094.4)"
-              d="M-1284.2-1155.8h99.106c3.11 0 5.613 2.028 5.613 4.546v15.612c0 2.519-2.504 4.547-5.613 4.547h-99.106c-3.11 0-5.613-2.028-5.613-4.547v-15.612c0-2.518 2.504-4.546 5.613-4.546z"
-              clipPath="url(#clipath_lpe_path-effect19757)"
-              fill="#333"
-              strokeLinejoin="round"
-              strokeWidth={1.008}
-              stroke="#4a4a4a"
-            />
-            <path
-              id="path5637-9-3-0"
-              d="M305.13 275.38h98.002"
-              fill="none"
-              strokeWidth={0.5321}
-              stroke="#4a4a4a"
-            />
-            <path
-              id="rect36772-7-1-5-1-0-08-4"
-              fill="#939d93"
-              paintOrder="markers stroke fill"
-              strokeWidth={0.13689}
-              stroke="#000"
-              d="M376.48 279.36H378.135V328.35H376.48z"
-            />
-            <path
-              id="path22961-5-9-0-5-1-0-3-7-0-6"
-              d="M338.35 318.92h38.362"
-              fill="#939d93"
-              strokeWidth={0.99383}
-              stroke="#939d93"
-            />
-            <ellipse
-              id="path19632"
-              cx={326.73}
-              cy={290.69}
-              rx={11.512}
-              ry={11.275}
-              fill="#0f0"
-            />
-            <text
-              id="text2160-2-7-8-0-4-8-7-0-3"
-              transform="scale(1.1716 .85351)"
-              x={271.54193}
-              y={345.09695}
-              style={{ lineHeight: 1.25 }}
-              xmlSpace="preserve"
-              fill="#000"
-              fontFamily="sans-serif"
-              fontSize="12.539px"
-              letterSpacing={0}
-              strokeWidth={0.31348}
-              wordSpacing={0}
-            >
-              <tspan
-                id="tspan2158-7-1-1-87-4-9-7-0-0"
-                x={271.54193}
-                y={345.09695}
-                fill="#000"
-                strokeWidth={0.31348}
-              >
-                {"F1"}
-              </tspan>
-            </text>
-            <ellipse
-              id="ellipse19634"
-              cx={326.73}
-              cy={318.37}
-              rx={11.512}
-              ry={11.275}
-              fill="#999"
-            />
-            <text
-              id="text2160-2-7-8-0-4-8-7-6-6-1"
-              transform="scale(1.1716 .85351)"
-              x={271.28171}
-              y={378.00323}
-              style={{ lineHeight: 1.25 }}
-              xmlSpace="preserve"
-              fill="#000"
-              fontFamily="sans-serif"
-              fontSize="12.539px"
-              letterSpacing={0}
-              strokeWidth={0.31348}
-              wordSpacing={0}
-            >
-              <tspan
-                id="tspan2158-7-1-1-87-4-9-7-4-8-9"
-                x={271.28171}
-                y={378.00323}
-                fill="#000"
-                strokeWidth={0.31348}
-              >
-                {"F2"}
-              </tspan>
-            </text>
-            <path
-              id="path5026"
-              d="M338.35 289.83h38.362"
-              fill="#939d93"
-              strokeWidth={0.99383}
-              stroke="#939d93"
-            />
-            <path
-              id="path17164-6-8-6-3"
-              d="M347.43 281.63l.491 18.094 18.677-10.052z"
-              fill="#0f0"
-            />
-            <path
-              id="path11127"
-              d="M347.43 310.52l.491 18.094 18.677-10.052z"
-              fill="#999"
-            />
-          </g>
-          <text
-            id="text2160-2-3-9-42-9-9-2-1"
-            transform="scale(1.0191 .98121)"
-            x={281.50357}
-            y={267.53741}
-            style={{ lineHeight: 1.25 }}
-            xmlSpace="preserve"
-            fill="#fff"
-            fontFamily="Mukti Narrow"
-            fontSize="15.354px"
-            fontWeight={700}
-            letterSpacing={0}
-            strokeWidth={0.2715}
-            wordSpacing={0}
-          >
-            <tspan
-              id="tspan2158-7-4-2-77-5-1-1-5"
-              x={281.50357}
-              y={267.53741}
-              fill="#fff"
-              fontFamily="sans-serif"
-              fontSize="15.354px"
-              fontWeight={700}
-              strokeWidth={0.2715}
-            >
-              {"ATS-PDU-10A"}
-            </tspan>
-          </text>
-        </g>
-      </g>
+      
       <g id="g18382" transform="translate(202.86 53.002) scale(.14245)">
         <g id="g18674" transform="translate(-117.36 -46.08)">
           <g id="g5070" transform="matrix(1.1584 0 0 1.2816 -40.659 -33.737)">
@@ -15914,6 +15732,9 @@ const uni = () => {
     />
     <Var23
       tupsout02A = {uni_23.tupsout02A}
+    />
+    <Var24
+      atspdu10A = {uni_24.atspdu10A}
     />
 
 
