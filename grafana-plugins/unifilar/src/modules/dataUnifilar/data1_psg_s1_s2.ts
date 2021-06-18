c5597e5e23c545d67b23670ad7cc377d82fc8028
bryanind195 <bryanind195@gmail.com>
2021-06-18 16:19:14 -0500

    se comienza con la creacion de los stylos de animacion de estados

--- grafana-plugins/unifilar/src/modules/dataUnifilar/data1_psg_s1_s2.ts
+++ grafana-plugins/unifilar/src/modules/dataUnifilar/data1_psg_s1_s2.ts
@@ -1,17 +1,18 @@
 import { DataPSG_1A_2A } from 'components/Variables_pincipales/var1_PSG_s1_s2'
+import breakersStyles from 'styles/breakersStyles'
 
 const dataPSG_1A_2A = (): DataPSG_1A_2A => {
   let dataPSG_1A_2A: DataPSG_1A_2A = {
     psg_1A_2A:{
-      psg01A_1: '',
-      psg01A_2: '',
-      psg01A_3: '',
+      psg01A_1: breakersStyles.sinConexion,
+      psg01A_2: breakersStyles.sinConexion,
+      psg01A_3: breakersStyles.sinConexion,
       gen1: '', 
       gen2: '', 
       gen3: '', 
-      psg02A_1: '',
-      psg02A_2: '',
-      psg02A_3: '',
+      psg02A_1: breakersStyles.sinConexion,
+      psg02A_2: breakersStyles.sinConexion,
+      psg02A_3: breakersStyles.sinConexion,
       gen4: '', 
       gen5: '', 
       gen6: ''
